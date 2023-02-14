cb99eff59dfa892aae8f42f9b18df7c8f9de2ce2
AndersWoodruff <113076762+AndersWoodruff@users.noreply.github.com>
2023-02-14 13:59:47 -0500

    starting file

---
+++ Blank.js
@@ -0,0 +1,5 @@
+// import ICALParser from 'ical-js-parser';
+// var input = document.createElement('input');
+
+// input.type = 'file';
+// input.click();
---
+++ background.js
@@ -0,0 +1,27 @@
+import ICALParser from 'ical-js-parser';
+var input = document.createElement('input');
+
+input.type = 'file';
+input.click();
+
+
+
+
+// const textbox = document.getElementById("body");
+// // function function1(){
+// //     textbox.style.backgroundColor = "lightGrey";
+// // }
+// // function function2(){
+// //     textbox.style.backgroundColor = "white";
+// // }
+
+// // texbox.addEventListener("mouseover",function(){
+// //     this.style.background = "#0000ff";
+// // });
+// // texbox.addEventListener("mouseout",function(){
+// //     this.style.background = "#ff0000";
+// // });
+// // textbox.onmouseover = function1();
+// // textbox.onmouseleave = function2();
+// // object.addEventListener("mouseover", function1);
+// // object.addEventListener("mouseleave", function2);
